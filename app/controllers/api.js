51e6ab0c49f77a38abf558373decb5ef76a687ae
Tom Söderlund <tom@tomsoderlund.com>
2014-11-28 10:38:15 +0100

    Password required to create

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -4,14 +4,12 @@ var Update = mongoose.model('Update');
 module.exports = {
 
 	list: function (req, res, next) {
-
 		var searchQuery = {};
 		if (req.query.from) {
 			searchQuery = { dateCreated: {"$gte": new Date(req.query.from)} };
 		}
-		console.log('API', searchQuery);
 
-		Update.find(searchQuery, null, { sort: {dateCreated: 1} }, function (err, updates) {
+		Update.find(searchQuery, null, { sort: {dateCreated: -1} }, function (err, updates) {
 			if (err) {
 				return res.json(400, err);
 			}
@@ -22,17 +20,22 @@ module.exports = {
 	},
 
 	// Create new update
-	// curl -X POST -H "Content-Type: application/json" -d '{ "title": "My title", "text": "Bla bla bla", "reloadNeeded": false }' http://localhost:3002/api/updates
+	// curl -X POST -H "Content-Type: application/json" -d '{ "title": "My title", "text": "Bla bla bla", "reloadNeeded": false }' http://localhost:3002/api/updates?password=M4EgsuY7PDZi
 	create: function (req, res, next) {
-		var newUpdate = new Update(req.body);
-		newUpdate.save(function (err) {
-			if (err) {
-				return res.json(400, err);
-			}
-			else {
-				return res.json(newUpdate);
-			}
-		});
+		if (req.query.password === 'M4EgsuY7PDZi') {
+			var newUpdate = new Update(req.body);
+			newUpdate.save(function (err) {
+				if (err) {
+					return res.json(400, err);
+				}
+				else {
+					return res.json(newUpdate);
+				}
+			});
+		}
+		else {
+			return res.json(401, 'Unauthorized');
+		}
 	},
 
 	// Delete update
